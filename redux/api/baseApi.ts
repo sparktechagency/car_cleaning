4007abe7944d31b46101d4b6c115e3892e1a86d7
Amite55 <amite403@gmail.com>
2025-07-08 16:03:36 +0600

    added base url live domain

--- redux/api/baseApi.ts
+++ redux/api/baseApi.ts
@@ -23,7 +23,7 @@ const baseQueryWithRath: BaseQueryFn<BaseQueryArgs, unknown, unknown> = async (
     // console.log(token, "token from base url............");
 
     const result: AxiosResponse = await axios({
-      baseURL: "http://103.186.20.114:8099/api",
+      baseURL: "http://72.167.47.160/api",
       // baseURL: "http://157.245.63.191/api",
       ...args,
       url: args.url,
@@ -86,4 +86,4 @@ export const api = createApi({
 });
 
 // export const imageUrl = 'http://192.168.12.160:7000/';
-export const imageUrl = "http://157.245.63.191/";
+export const imageUrl = "http://72.167.47.160/";
